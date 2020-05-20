3f9547134e26636126a1ddff289d528d1c654f1a
Faye <mendoza.fatimah@gmail.com>
2020-05-20 17:13:04 +1200

    initial project file and structure

---
+++ Assets/Scripts/CS_playerMovement.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class CS_playerMovement : MonoBehaviour
+{
+    //private CharacterController player;
+    private Rigidbody rbPlayer;
+
+    private float accelInput;
+    private float turnInput;
+    private float carSpeed;
+    private float turnSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //player = this.GetComponent<CharacterController>();
+        rbPlayer = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get values inputted by player
+        accelInput = Input.GetAxis("Vertical");
+        turnInput = Input.GetAxis("Horizontal");
+
+        carSpeed = 100;
+        turnSpeed = 20;
+        
+
+    }
+
+    void FixedUpdate()
+    {
+
+        //movement of player vehicle
+        VehicleMovement();
+
+    }
+
+    void LateUpdate()
+    {
+
+    }
+
+    private void VehicleMovement()
+    {
+        //move forward / accelerate
+        if (accelInput != 0)
+        {
+            rbPlayer.velocity = transform.forward * carSpeed * accelInput;
+        }
+
+
+        //turn left and right
+        if (turnSpeed !=0)
+        {
+            this.transform.Rotate(new Vector3(0, turnInput, 0) * turnSpeed * Time.deltaTime);
+        }
+    }
+
+    private void VehicleAnimations()
+    {
+
+    }
+}
---
+++ Assets/Scripts/CS_playerParameters.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CS_playerParameters : MonoBehaviour
+{
+    //base parameters for player vehicles
+    private float baseSpeed;
+    private float baseHealth;
+
+    //actual parameter values for instantiated vehicles
+    public float Speed;
+    public float Health;
+
+    public static void SetBasicPlayerParameters()
+    {
+        
+    }
+}
